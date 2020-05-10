5c73b4e12cdd9e5653379117b5bc197dbb902509
Jozef Chovanec <36828902+JozefChovanecMT@users.noreply.github.com>
2020-05-10 20:41:52 +0200

    Update ganja_light.ino

--- ganja_light/ganja_light.ino
+++ ganja_light/ganja_light.ino
@@ -21,7 +21,7 @@
 //  0. You just DO WHAT THE FUCK YOU WANT TO.                                   
 //
 
-#define A_MODE_BRIGHT .55 // svietivosť v % za bodkou.
+#define A_MODE_BRIGHT .54 // svietivosť v % za bodkou.
 
 #define PWM_OUT_PIN 5 // pin na riadenie lediek
 
